88f2c3b19ea8b55751671f00b6808827ffba784c
Popenkov Anton <65846634+popenkov@users.noreply.github.com>
2021-08-16 11:17:32 +0300

    practising forms

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,9 +1,45 @@
 import React, {Component} from 'react'
+import classes from './Auth.module.css'
+import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/Input/Input'
 
 export default class Auth extends Component {
+    loginHandler = () => {
+        
+    }
+
+    registerHandler = () => {
+
+    }
+
+    submitHandler = evt => {
+        evt.preventDefault();
+    }
+
+    
+    
     render() {
         return (
-            <h1>Auth</h1>  
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Authorisation</h1> 
+                    
+                    <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+                        <Input  label='Email' 
+
+                        />
+                        <Input  
+                            label='Password' 
+                            errorMessage = 'test'
+                        />
+                        
+
+                        <Button type='success' onClick={this.loginHandler}>Log in</Button>
+                        <Button type='primary' onClick={this.registerHandler}>Register</Button>
+                        
+                    </form>
+                </div> 
+            </div>
         )
     }
 }
