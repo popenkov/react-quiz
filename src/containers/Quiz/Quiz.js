fea9dd0565b1e7a47bedb85b2cca5935a6179f95
Popenkov Anton <65846634+popenkov@users.noreply.github.com>
2021-09-14 11:51:29 +0300

    changing state to redux

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -2,64 +2,15 @@ import React, {Component} from 'react'
 import classes from './Quiz.module.css'
 import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 import FinishedQuiz from '../../components/FinishedQuiz/FinishedQuiz' 
-import axios from '../../axios/axios-quiz';
 import Loader from '../../components/UI/Loader/Loader';
 import { connect } from 'react-redux'
-import { fetchQuizById } from '../../store/actions/quiz'
+import { fetchQuizById, quizAnswerClick } from '../../store/actions/quiz'
 
 
 class Quiz extends Component {
 
     onAnswerClickHandler = (answerId) => {
-        if (this.state.answerState) {
-            //Метод Object.keys() возвращает массив из собственных 
-            //перечисляемых свойств переданного объекта
-            const key = Object.keys(this.state.answerState)[0];
-            //проверка чтобы не завершать игру несколькими кликами по правильному ответу старого вопроса
-            if (this.state.answerState[key] === 'success') {
-                return;
-            }
-        }
-
-        const question = this.state.quiz[this.state.activeQuestion]
-        const results = this.state.results
-
-        if (question.rightAnswerId === answerId) {
-            if (!results[question.id]) { //если объект пустой
-                results[question.id] = 'success';
-            } 
-            this.setState({
-                answerState: {[answerId]: 'success'} ,
-                results
-            })
-            
-
-            //чтобы в течение секунды показать, что ответ правильный
-            const timeout = window.setTimeout(() => {
-                //проверка, закончились ли вопросы
-                if (this.isQuizFinished()) {
-                    
-                    this.setState({
-                        isFinished: true
-                    })
-                } else {
-                    this.setState({
-                        activeQuestion: this.state.activeQuestion + 1,
-                        answerState: null
-                    })
-                }
-
-                window.clearTimeout(timeout);
-            }, 1000)
-
-        } else {
-            results[question.id] = 'error';
-            this.setState({
-                answerState: {[answerId]: 'error'},
-                results: results //или просто results
-            })
-
-        } 
+       dispatch(quizAnswerClick(answerId))
     }
 
     startNewGame = () => {
@@ -126,6 +77,7 @@ function mapStateToProps (state) {
 function mapDispatchToProps (dispatch) {
     return {
         fetchQuizById: id => dispatch(fetchQuizById(id)),
+        quizAnswerClick: answerId => dispatch(quizAnswerClick(answerId))
 
     }
 }
--- src/store/actions/quiz.js
+++ src/store/actions/quiz.js
@@ -74,3 +74,63 @@ export function fetchQuizesError (e) {
         error: e
     }
 }
+
+export function quizSetState () {
+
+}
+
+export function quizAnswerClick(answerId) {
+    return (dispatch, getState) => {
+        const state = getState().quiz
+        if (state.answerState) {
+            //Метод Object.keys() возвращает массив из собственных 
+            //перечисляемых свойств переданного объекта
+            const key = Object.keys(state.answerState)[0];
+            //проверка чтобы не завершать игру несколькими кликами по правильному ответу старого вопроса
+            if (state.answerState[key] === 'success') {
+                return;
+            }
+        }
+
+        const question = state.quiz[this.state.activeQuestion]
+        const results = state.results
+
+        if (question.rightAnswerId === answerId) {
+            if (!results[question.id]) { //если объект пустой
+                results[question.id] = 'success';
+            } 
+            dispatch(quizSetState())
+      /*       this.setState({
+                answerState: {[answerId]: 'success'} ,
+                results
+            }) */
+            
+
+            //чтобы в течение секунды показать, что ответ правильный
+            const timeout = window.setTimeout(() => {
+                //проверка, закончились ли вопросы
+                if (this.isQuizFinished()) {
+                    
+                   /*  this.setState({
+                        isFinished: true
+                    }) */
+                } else {
+ /*                    this.setState({
+                        activeQuestion: this.state.activeQuestion + 1,
+                        answerState: null
+                    }) */
+                }
+
+                window.clearTimeout(timeout);
+            }, 1000)
+
+        } else {
+            results[question.id] = 'error';
+/*             this.setState({
+                answerState: {[answerId]: 'error'},
+                results: results //или просто results
+            }) */
+
+        } 
+    }
+}
